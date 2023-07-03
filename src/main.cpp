b14f3b3ff5180caa7dd94c8ee6b30a41b33a8129
manish acharya <mansih.acharya313@gmail.com>
2023-07-03 14:38:17 +0545

    sfml simple game

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <SDL.h>
+#include <iostream>
+#include <sdl/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello world" << std::endl;
+    std::cin.get();
+    return 0;
+}
